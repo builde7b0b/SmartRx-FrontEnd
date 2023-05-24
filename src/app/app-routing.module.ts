520f450f81b4ac522349677193700cd52cd635be
builde7b0b <ggives3@gmail.com>
2023-05-24 16:17:35 -0700

    define imports for components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { PrescriptionListComponent } from './prescription-list/prescription-list.component';
+import { RefillRequestComponent } from './refill-request/refill-request.component';
+
+
 
 const routes: Routes = [];
 
