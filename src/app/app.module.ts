59b86caf33eb330d010fc3ed6a4a0effe104a785
builde7b0b <ggives3@gmail.com>
2023-05-24 16:12:52 -0700

    prescription list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { PrescriptionListComponent } from './prescription-list/prescription-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    PrescriptionListComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/prescription-list/prescription-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-prescription-list',
+  templateUrl: './prescription-list.component.html',
+  styleUrls: ['./prescription-list.component.css']
+})
+export class PrescriptionListComponent {
+
+}
